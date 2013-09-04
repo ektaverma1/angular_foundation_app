7992499d67afc0158d4643f565c5284fa3d7b7d0
Ekta Verma <eku4evr@gmail.com>
2013-09-04 02:05:16 -0700

    initial commit with foundation

---
+++ app/views/people/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @person, :name, :designation, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularFoundationApp::Application.config.secret_key_base = 'de357cf17118beb19692641a72185624901346c34eb8aba5420a6d6e61f83130734e4314b5c006874f9b647da790833a64028665658b2779f3830f4590f1aa00'
